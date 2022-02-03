3357dff55bf2ae05a3003474d686cc34a1c6ea26
ESOLUTIONS\cshenoy <chaithrashenoy33@gmail.com>
2022-02-03 13:21:33 +0530

    commented 2 lines

--- src/test/java/testing/testing/TestSeleniumJenkins.java
+++ src/test/java/testing/testing/TestSeleniumJenkins.java
@@ -16,8 +16,8 @@ public class TestSeleniumJenkins extends TestCase {
 		driver.get("https://www.google.com/");
 		System.out.println(driver.getTitle());
 		assertEquals("Google", driver.getTitle());
-        driver.manage().window().maximize();
-		Thread.sleep(3000);
+       // driver.manage().window().maximize();
+		// Thread.sleep(5000);
 		driver.close();
 	}
 
